e6a6ad9e920f389e3a919c1d6edcdc4dffcab74d
Javier Cano <javier.cano.montero@urjc.es>
2017-02-26 19:58:51 +0100

    Update aux_functions.R

--- R/aux_functions.R
+++ R/aux_functions.R
@@ -2,7 +2,7 @@ getData <- function(){
   for(i in 1:12)
   {
     download.file(paste0("https://transtats.bts.gov//PREZIP/On_Time_On_Time_Performance_2016_",i,".zip"),paste0("data/Performance_2016_",i,".zip"))
-    unzip(paste0("data/Performance_2016_",i,".zip"),overwrite=T, exdir = "data")
+    unzip(paste0("../data/Performance_2016_",i,".zip"),overwrite=T, exdir = "data")
   }
 }
 
@@ -10,8 +10,8 @@ mergeFiles <- function(){
   perf <- c()
   for(i in 1:12)
   {
-    aux <- fread(paste0("data/On_Time_On_Time_Performance_2016_",i,".csv"), data.table = F, stringsAsFactors = T)
+    aux <- fread(paste0("../data/On_Time_On_Time_Performance_2016_",i,".csv"), data.table = F, stringsAsFactors = T)
     perf <- rbind(perf,aux)
   }
-  write.csv(perf,"data/On_Time_On_Time_Performance_2016_full.csv", row.names = F, col.names = T)
+  write.csv(perf,"../data/On_Time_On_Time_Performance_2016_full.csv", row.names = F, col.names = T)
 }
